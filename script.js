d03cd69f763e7e9965dc18c379a69e999bfc9e13
Arseniy <seny_2978@mail.ru>
2022-01-13 21:31:08 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict"
+
+// function User(name, id){
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log(` Hello ${this.name}`);
+//     }
+// };
+
+// const ivan = new User('Ivan', 29);
+
+// const alex = new User('Alex',  21);
+
+// ivan.hello();
+// alex.hello();
+
+// console.log(ivan);
+// console.log(alex);
+
+// function pow(x , n) {
+//     return x ** n;
+// };
+
+// console.log(pow(2, 3));
+
